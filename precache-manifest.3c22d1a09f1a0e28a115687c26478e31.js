9fcf6dbb051894dff4d7928447e4c3dea458d808
Hayen Phan <hayenphan@MacBook-Pro-van-Hayen.local>
2020-06-17 01:10:26 +0200

    Updates

---
+++ precache-manifest.3c22d1a09f1a0e28a115687c26478e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df1c4c67dfe02f3ecd0fba59e7a974d",
+    "url": "/ILE-Erasmus-Deploy/index.html"
+  },
+  {
+    "revision": "e4579515d37f41b125d7",
+    "url": "/ILE-Erasmus-Deploy/static/css/main.2ba0ca69.chunk.css"
+  },
+  {
+    "revision": "dfb723459ff545812f1e",
+    "url": "/ILE-Erasmus-Deploy/static/js/2.eae45f6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ILE-Erasmus-Deploy/static/js/2.eae45f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4579515d37f41b125d7",
+    "url": "/ILE-Erasmus-Deploy/static/js/main.1e0327dc.chunk.js"
+  },
+  {
+    "revision": "b8e0c4ab6213cbfcaad7",
+    "url": "/ILE-Erasmus-Deploy/static/js/runtime-main.dd8bcb2d.js"
+  },
+  {
+    "revision": "fd136912f851ae1e6f81511b3dd11acd",
+    "url": "/ILE-Erasmus-Deploy/static/media/dicom-pelvic.fd136912.jpg"
+  }
+]);

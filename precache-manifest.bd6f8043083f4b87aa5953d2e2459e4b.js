6fde4e3b0cf85bffdf5c9609d81256748e404fcc
Hayen Phan <hayenphan@MacBook-Pro-van-Hayen.local>
2020-06-17 01:16:21 +0200

    Updates

---
+++ precache-manifest.bd6f8043083f4b87aa5953d2e2459e4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b1562cfa3c0945c12d308fb76f8e9a",
+    "url": "/ILE-Erasmus-Deploy.github.io/index.html"
+  },
+  {
+    "revision": "66109ded67dd34b30e34",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/css/main.2ba0ca69.chunk.css"
+  },
+  {
+    "revision": "dfb723459ff545812f1e",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/js/2.eae45f6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/js/2.eae45f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66109ded67dd34b30e34",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/js/main.881cc3a7.chunk.js"
+  },
+  {
+    "revision": "6cd5d8cca48f8b149f45",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/js/runtime-main.63ef04c5.js"
+  },
+  {
+    "revision": "fd136912f851ae1e6f81511b3dd11acd",
+    "url": "/ILE-Erasmus-Deploy.github.io/static/media/dicom-pelvic.fd136912.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba754fb836cc1412f1a8f0aa6fc9e8f6.js"
+  "/ILE-Erasmus-Deploy.github.io/precache-manifest.bd6f8043083f4b87aa5953d2e2459e4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ILE-Erasmus-Deploy.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
